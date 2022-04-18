3169ab08c2d539427f6c4850f8f1866c6dc3c44c
nosleepexe <95152560+nosleepexe@users.noreply.github.com>
2022-04-17 23:49:42 -0400

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -35,4 +35,18 @@ public class Sean_X_Drive
   double y = gamepad1.left_stick_y;
   double x = gamepad1.right_stick_x * 1.1;
   double rx = gamepad1.left_stick_x;
-}    
+   
+    double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+    double frontLeftPower = (y + x + rx) / denominator;
+    double backLeftPower = (y - x + rx) / denominator;
+    double frontRightPower = (y - x - rx) / denominator;
+    double backRightPower = (y + x - rx) / denominator;
+
+   frontLeft.setPower(frontLeftPower);
+   backLeft.setPower(backLeftPower);
+   frontRight.setPower(frontRightPower);
+   backRight.setPower(backRightPower);
+  }
+  idle();
+ }
+}
